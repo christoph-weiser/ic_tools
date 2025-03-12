62fabf738e8702b45016763054c276041d567afe
christoph <christoph-weiser@gmx.de>
2025-03-12 20:10:35 +0100

    added ihp lvs setup

---
+++ lvs/lvs_ihp130.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#------------------------------------------------------------
+# NAME
+#       lvs.sh
+#
+# SYNOPSIS
+#       lvs.sh file_a file_b
+#
+# DESCRIPTION
+#       perform lvs using netgen between two spice
+#       files.
+#
+#------------------------------------------------------------
+
+file_a="$1"
+file_b="$2"
+
+[[ -z "$file_a" ]] || [[ -z "$file_b" ]] \
+&& echo "Usage: lvs.sh file_a file_b" && exit 0
+
+netgen -batch lvs "$file_a" "$file_b" "$IC_TOOLS/lvs/ihp-sg13g2_setup.tcl"
+
+
