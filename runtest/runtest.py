f4b575d107b50bc2814ea54465d39bf8de3edddd
christoph <christoph-weiser@gmx.de>
2026-01-09 23:16:38 +0100

    added runtest to ic tools

---
+++ runtest/runtest.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import logging
+import multiprocessing
+import spctl
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source",
+                        type=str,
+                        help="config file with case setup")
+    parser.add_argument("--cores",
+                        type=str,
+                        default=1,
+                        help="number of cores for multiprocess")
+    parser.add_argument("--simulator",
+                        type=str,
+                        default="ngspice",
+                        help="which simulator to use (ngspice,xyce)")
+                        
+    args = parser.parse_args()
+
+
+    cores = int(args.cores)
+    configfile = args.source
+    simulator = args.simulator
+
+    cases, paths = spctl.setup(configfile)
+  
+    paths["file_xschemrc"] = os.getenv("XSCHEMRC")
+    paths["path_corners"]  = os.getenv("CORNERS")
+
+  
+    paths["file_netlist"] = spctl.create_netlist(paths["file_schematic"],
+                                                 paths["path_netlists"],
+                                                 paths["file_xschemrc"])
+  
+    with multiprocessing.Manager() as manager:
+        result_queue = manager.Queue()
+        writer = multiprocessing.Process(target=spctl.file_writer, 
+                                         args=(result_queue, 
+                                         paths["file_summary"]))
+        writer.start()
+        with multiprocessing.Pool(processes=cores) as pool:
+            args_list = [(paths, c, result_queue, simulator) for c in cases]
+            pool.starmap(spctl.run_cases, args_list)
+        result_queue.put("exit")
+        writer.join()
---
+++ runtest/runtest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#------------------------------------------------------------
+# NAME
+#       runtest.sh
+#
+# SYNOPSIS
+#       runtest.sh file [cores]
+#
+# DESCRIPTION
+#       initiate a regression test based on 
+#       the setup specified in the config file.
+#
+#------------------------------------------------------------
+
+script="$IC_TOOLS/runtest/runtest.py"
+
+file="$1"
+cores="$2"
+
+[[ -z "$1" ]] && echo "runtest.sh file [cores]" && exit
+
+if [ -z "$2" ]; then 
+    cores=1
+fi
+
+python "$script" --source="$file" --cores="$cores"
